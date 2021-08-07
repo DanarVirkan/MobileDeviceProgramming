d08d766a4e238cd73ba730c54f08757af973383e
DanarVirkan <danarvirkan@gmail.com>
2021-08-07 22:07:33 +0700

    Added LoginActivity.kt for login function

---
+++ app/src/main/java/com/example/mobiledeviceprogramming/presentation/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mobiledeviceprogramming.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobiledeviceprogramming.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            if (binding.loginUsername.editText!!.text.toString() == "admin" && binding.loginPassword.editText!!.text.toString() == "admin"
+            ) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                binding.loginUsername.error = "Username salah"
+                binding.loginPassword.error = "Password salah"
+            }
+        }
+    }
+}
